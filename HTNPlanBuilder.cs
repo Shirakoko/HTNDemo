e9b121f196c7481d5e5f1e8f0c6ecb4fb9155850
matsuki-aFHZE <1971173893@qq.com>
2025-02-18 12:30:06 +0800

    1.修改构造器的注释；2.Add GameManager

--- HTNPlanBuilder.cs
+++ HTNPlanBuilder.cs
@@ -13,19 +13,21 @@ public partial class HTNPlanBuilder
     
     private void AddTask(IBaseTask task)
     {
-        if (planner != null)//当前计划器不为空
+        if (planner != null)
         {
-            //将新任务作为构造栈顶元素的子任务
+            // 当前计划器不为空，将新任务作为构造栈顶元素的子任务
             taskStack.Peek().AddNextTask(task);
         }
-        else //如果计划器为空，意味着新任务是根任务，进行初始化
+        else 
         {
+            // 当前计划器为空，初始化规划器和执行器
             planner = new HTNPlanner(task as CompoundTask);
             runner = new HTNPlanRunner(planner);
         }
-        //如果新任务是原子任务，就不需要进栈了，因为原子任务不会有子任务
+        
         if (task is not PrimitiveTask)
         {
+            //如果新任务是原子任务，就不需要进栈了，因为原子任务不会有子任务
             taskStack.Push(task);
         }
     }
@@ -45,14 +47,21 @@ public partial class HTNPlanBuilder
         return planner;
     }
 
-    // 添加原子任务
+    /// <summary>
+    /// 添加原子任务
+    /// </summary>
+    /// <param name="task">原子任务</param>
+    /// <returns>构造器本身</returns>
     public HTNPlanBuilder AddPrimitiveTask(PrimitiveTask task)
     {
         AddTask(task);
         return this;
     }
 
-    // 添加复合任务
+    /// <summary>
+    /// 添加复合任务
+    /// </summary>
+    /// <returns>构造器本身</returns>
     public HTNPlanBuilder AddCompoundTask()
     {
         var task = new CompoundTask();
@@ -60,7 +69,11 @@ public partial class HTNPlanBuilder
         return this;
     }
 
-    // 添加方法
+    /// <summary>
+    /// 添加方法
+    /// </summary>
+    /// <param name="condition">方法执行的条件</param>
+    /// <returns>构造器本身</returns>
     public HTNPlanBuilder AddMethod(System.Func<bool> condition)
     {
         var task = new Method(condition);
