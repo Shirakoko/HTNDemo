58fee68404b43a8bd9f0e4d9bdf8b6a4f625323f
Yuki <97279549+Shirakoko@users.noreply.github.com>
2025-02-18 13:11:41 +0800

    修改planner和runner的注释

--- HTNPlanRunner.cs
+++ HTNPlanRunner.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class HTNPlanRunner
 {
-    //当前运行状态
+    // 当前运行状态
     private EStatus curState;
-    //直接将规划器包含进来，方便重新规划
+    // 直接将规划器包含进来，方便重新规划
     private readonly HTNPlanner planner;
-    //当前执行的原子任务
+    // 当前执行的原子任务
     private PrimitiveTask curTask;
-    //标记「原子任务列表是否还有元素、能够继续」
+    // 标记「原子任务列表是否还有元素、能够继续」
     private bool canContinue;
 
     public HTNPlanRunner(HTNPlanner planner)
@@ -19,28 +19,25 @@ public class HTNPlanRunner
     
     public void RunPlan()
     {
-        //如果当前运行状态是失败（一开始默认失败）
         if(curState == EStatus.Failure)
         {
-            //就规划一次
+            // 如果当前运行状态是失败（一开始默认失败）就规划一次
             planner.Plan();
         } else if(curState == EStatus.Success)
         {
-            //如果当前运行状态是成功，就表示当前任务完成了，让当前原子任务造成影响
-            // Debug.Log($"任务{curTask.GetTaskName()}执行成功，将对世界状态造成影响");
+            // 如果当前运行状态是成功，就表示当前任务完成了，让当前原子任务造成影响
             curTask.Effect();
         }
-        /*如果当前状态不是「正在执行/EStatus.Running」，就取出新一个原子任务作为当前任务
-        无论失败还是成功，都要这么做。因为如果是失败，肯定在代码运行到这
-        之前，已经进行了一次规划，理应获取新规划出的任务来运行；如果是因
-        为成功，那也要取出新任务来运行*/
+        /* 如果当前状态不是「正在执行/EStatus.Running」，就取出新一个原子任务作为当前任务
+        如果「失败/EStatus.Failure」，到这之前已经进行了一次规划，理应获取新规划出的任务来运行；
+        如果「成功/EStatus.Success」，也要取出新任务来运行 */
         if(curState != EStatus.Running)
         {
             //用Pop的返回结果判断规划器的FinalTasks是否为空
             canContinue = planner.FinalTasks.TryPop(out curTask);
         }
 
-        /*如果canContinue为false（即curTask为null）也视作失败（其实是「全部完成」，但全部完成和失败是一样的，都要重新规划）。
+        /* 如果canContinue为false（即curTask为null）也视作失败（其实是「全部完成」，但全部完成和失败是一样的，都要重新规划）。
         只有当canContinue && curTask.MetCondition()都满足时，才读取当前原子任务的运行状态，否则就失败。*/
         if (canContinue && curTask != null)
         {
--- HTNPlanner.cs
+++ HTNPlanner.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class HTNPlanner
 {
-    //最终分解完成的所有原子任务存放的列表
+    // 最终分解完成的所有原子任务存放的列表
     public Stack<PrimitiveTask> FinalTasks { get; private set; }
-    //辅助栈，分解过程中，用来缓存被分解出的任务的栈，因为类型各异，故用IBaseTask类型
+
+    // 分解过程中，用来缓存被分解出的任务（复合任务和原子任务）的栈
     private readonly Stack<IBaseTask> taskOfProcess;
     private readonly CompoundTask rootTask; //根任务
 
@@ -17,46 +18,46 @@ public class HTNPlanner
         FinalTasks = new Stack<PrimitiveTask>();
     }
 
-    //规划（核心）
+    /// <summary>
+    /// 执行规划（核心逻辑）
+    /// </summary>
     public void Plan()
     {
-        //先复制一份世界状态
+        // 复制一份世界状态
         var worldState = HTNWorld.CopyWorldState();
-        //将存储列表清空，避免上次计划结果的影响
+        // 将存储列表清空，避免上次计划结果的影响
         FinalTasks.Clear();
-        //将根任务压进栈中，准备分解
+        // 将根任务压进栈中，准备分解
         taskOfProcess.Push(rootTask);
-        //只要栈还没空，就继续分解
+        // 只要栈还没空，就继续分解
         while(taskOfProcess.Count > 0)
         {
-            //拿出栈顶的元素
+            // 拿出栈顶的元素
             var task = taskOfProcess.Pop();
-            //如果这个元素是复合任务
+            // 元素是复合任务
             if(task is CompoundTask cTask)
             {
-                //判断是否可以执行
+                // 判断是否可以执行
                 if(cTask.MetCondition(worldState))
                 {
-                    /*如果可以执行，就肯定有可用的方法，
-                    就将该方法的子任务都压入栈中，以便继续分解*/
+                    // 如果可以执行，就肯定有可用的方法，将该方法的子任务都压入栈中，以继续分解
                     var subTask = cTask.ValidMethod.SubTask;
                     foreach(var t in subTask)
                     {
                         taskOfProcess.Push(t);
                     }
-                    /*通过上面的步骤我们知道，能被压进栈中的只有
-                    复合任务和原子任务，方法本身并不会入栈*/
+                    /* 通过上面的步骤可知，能被压进栈中的只有
+                    复合任务和原子任务，方法本身并不会入栈 */
                 }
             }
-            else //否则，这个元素就是原子任务
+            else
             {
-                //将该元素转为原子任务，因为原本是IBaseTask类型
+                // 元素是原子任务；将该元素转为原子任务加入存放分解完成的任务列表
                 var pTask = task as PrimitiveTask;
-                //再将该原子任务加入存放分解完成的任务列表
                 FinalTasks.Push(pTask);
             }
         }
-        // 将所有任务名称拼接成一行
+        
         string taskNames = string.Join(", ", FinalTasks.Select(task => task.GetTaskName()));
         Debug.Log($"计划完毕，队列中的任务有: {taskNames}");
     }
